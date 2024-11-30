aab8db57337960cab6ed4685be3fdbc3a6d58be3
or-abdillh <hans.abdillh05@gmail.com>
2024-11-30 21:21:54 +0800

    feat: add Authenticated and Base layout components with title template

---
+++ layouts/Authenticated.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+// instances
+const runtimeConfig = useRuntimeConfig()
+
+useHead({
+    titleTemplate(title) {
+        return title ? `${title} — ${runtimeConfig.public.appName}` : runtimeConfig.public.appName
+    },
+})
+
+</script>
+
+<template>
+    <div>
+        Layout: Authenticated
+        <slot />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ layouts/Base.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+// instances
+const runtimeConfig = useRuntimeConfig()
+
+useHead({
+    titleTemplate(title) {
+        return title ? `${title} — ${runtimeConfig.public.appName}` : runtimeConfig.public.appName
+    },
+})
+
+</script>
+
+<template>
+    <main class="min-h-screen box-border grid place-items-center bg-slate-50">
+        <slot />
+    </main>
+</template>
